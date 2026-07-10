1c851f92949ef849c227f464c93dc179e5a2d4dd
mn.ustiuzhanin <zaoooza33@gmail.com>
2026-07-10 16:09:24 +0700

    add selenoid

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -14,10 +14,10 @@ public class TestBase {
     static void setupConfig() {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+        Configuration.browserVersion = "148.0";
         Configuration.baseUrl = "https://new.pn.ru/";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.browser = "chrome";
-        Configuration.browserVersion = "148.0";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
